106d188926a14c8c2ddc87a649f9635e043bb05e
Charaka Yahampath <109475982+charakay@users.noreply.github.com>
2025-07-22 16:21:56 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hello from Choreo PHP Server</title>
+</head>
+<body>
+  <h1>Hello, World from Choreo!</h1>
+</body>
+</html>
+HTML;
+?>
